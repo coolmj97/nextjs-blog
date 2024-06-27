deda658cac6b3cecd4d02427d4adc5230570fd6f
Min Jeong Kim <minnjeong.kim@gmail.com>
2024-06-27 14:17:32 +0900

    마크다운 코드블록 css 추가

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,5 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -11,4 +12,16 @@ export const Post = defineDocumentType(() => ({
   },
 }));
 
-export default makeSource({ contentDirPath: 'posts', documentTypes: [Post] });
+const rehypeOptions = {
+  theme: 'slack-dark',
+  keepBackground: true,
+};
+
+export default makeSource({
+  contentDirPath: 'posts',
+  documentTypes: [Post],
+  mdx: {
+    //@ts-expect-error
+    rehypePlugins: [[rehypePrettyCode, rehypeOptions]],
+  },
+});
